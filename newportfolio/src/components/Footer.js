34d019a86f3f4ec42f72ff21ee764aec383b27eb
Griffin <griffincriste@gmail.com>
2020-08-13 22:34:05 -0500

    stretch image

--- newportfolio/src/components/Footer.js
+++ newportfolio/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
 	return (
 		<div id='header'>
-			<div className='container'>
+			<div className='container-fluid hero-container'>
 				<div className='row'>
 					<div className='col-md-12 col-12'>
 						<img className='heroImage' src={require('./assets/coding.jpeg')} />
--- newportfolio/src/components/Header.js
+++ newportfolio/src/components/Header.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Header() {
 	return (
 		<div id='header'>
-			<div className='container'>
+			<div className='container-fluid hero-container'>
 				<div className='row'>
 					<div className='col-md-12 col-12'>
-						<img className='heroImage' src={require('./assets/codecode.jpg')} />
+						<img className='heroImage expand-lg' src={require('./assets/codecode.jpg')} />
 					</div>
 				</div>
 			</div>
