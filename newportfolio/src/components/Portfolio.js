b80bd2007d93f730e5f7334e715178b9620de831
Griffin <griffincriste@gmail.com>
2020-09-10 21:12:42 -0500

    cta

--- newportfolio/src/components/Portfolio.js
+++ newportfolio/src/components/Portfolio.js
@@ -6,52 +6,47 @@ function Nav() {
 			<div classNameName='container'>
 				<div classNameName='row'>
 					<div className='col-md-12 col-12'>
-						<div className='card about-card'>
-							<h2 className='text-center'>Portfolio</h2>
+						<div className='card portfolio-card'>
+							<h2 className='text-center'>PORTFOLIO</h2>
 							<div id='portfolio' className='className bg-2'>
 								<div className='container text-center'>
-									<h3>PORTFOLIO</h3>
-
 									<div className='row'>
 										<div className='col-md-3 col-12'>
 											<div className='card well'>
 												<div className='card-header'>
 													<h4 className='text-center card-title'>
-														<strong>GOT Memory Game </strong>
+														<strong>Memory Game </strong>
 													</h4>
-													<div className='card-body'>
-														<a href='#demo2' data-toggle='collapse'>
-															<img
-																src={require('./assets/GOT.png')}
-																className='person'
-																alt='pic of game of thrones'
-															/>
-														</a>
-														<div id='demo2' className='collapse'>
-															<h5>
+												</div>
+												<div className='card-body'>
+													<a href='#demo2' data-toggle='collapse'>
+														<img
+															src={require('./assets/GOT.png')}
+															className='person img-fluid'
+															alt='pic of game of thrones'
+														/>
+													</a>
+													<div id='demo2' className='collapse'>
+														{' '}
+														<div className='link-container'>
+															<a
+																href='https://dry-journey-39353.herokuapp.com/'
+																className='portfolio-link'
+															>
+																<h5>Live Link</h5>
+															</a>{' '}
+															<a
+																href='https://github.com/Gcriste/clickygame'
+																className='portfolio-link'
+															>
 																{' '}
-																<a
-																	href='https://dry-journey-39353.herokuapp.com/'
-																	className='pichead'
-																>
-																	Live Link
-																</a>
-															</h5>
-															<h5>
-																{' '}
-																<a
-																	href='https://github.com/Gcriste/clickygame'
-																	className='pichead'
-																>
-																	{' '}
-																	GitHub Link
-																</a>
-															</h5>
-															<p className='app-description'>
-																{' '}
-																Simple memory game based on the theme of Game of Thrones
-															</p>
+																<h5>GitHub Link</h5>
+															</a>
 														</div>
+														<p className='app-description'>
+															{' '}
+															Simple memory game based on the theme of Game of Thrones
+														</p>
 													</div>
 												</div>
 											</div>
@@ -60,36 +55,32 @@ function Nav() {
 											<div className='card well'>
 												<div className='card-header'>
 													<h4 className='text-center card-title'>
-														<strong>Rural Community Credit Union</strong>
+														<strong>RCCU</strong>
 													</h4>
-													<div className='card-body'>
-														<a href='#demo' data-toggle='collapse'>
-															<img
-																src={require('./assets/RCCU.png')}
-																className='person'
-																alt='pic of Google'
-															/>
-														</a>
-														<div id='demo' className='collapse'>
-															<h5>
+												</div>
+												<div className='card-body'>
+													<a href='#demo' data-toggle='collapse'>
+														<img
+															src={require('./assets/RCCU.png')}
+															className='person img-fluid'
+															alt='pic of Google'
+														/>
+													</a>
+													<div id='demo' className='collapse'>
+														{' '}
+														<div className='link-container'>
+															<a href='https://www.rccu.com/' className='portfolio-link'>
 																{' '}
-																<a href='https://www.rccu.com/' className='pichead'>
-																	{' '}
-																	Live Link
-																</a>
-															</h5>
-															<h5>
+																<h5>Live Link</h5>
+															</a>{' '}
+															<a href='/' className='portfolio-link'>
 																{' '}
-																<a href='/' className='pichead'>
-																	{' '}
-																	GitHub Link{' '}
-																</a>{' '}
-															</h5>
-															<p className='app-description'>
-																A google search app that searches the google api for
-																books.
-															</p>
+																<h5>GitHub Link </h5>
+															</a>{' '}
 														</div>
+														<p className='app-description'>
+															A google search app that searches the google api for books.
+														</p>
 													</div>
 												</div>
 											</div>
@@ -100,39 +91,35 @@ function Nav() {
 													<h4 className='text-center card-title'>
 														<strong>Gig Finder </strong>
 													</h4>
-													<div className='card-body'>
-														<a href='#demo3' data-toggle='collapse'>
-															<img
-																src={require('./assets/GigFinder.png')}
-																className='person'
-																alt='pic of gigfinder'
-															/>
-														</a>
-														<div id='demo3' className='collapse'>
-															<h5>
+												</div>
+												<div className='card-body'>
+													<a href='#demo3' data-toggle='collapse'>
+														<img
+															src={require('./assets/GigFinder.png')}
+															className='person img-fluid'
+															alt='pic of gigfinder'
+														/>
+													</a>
+													<div id='demo3' className='collapse'>
+														{' '}
+														<div className='link-container'>
+															<a
+																href='https://newgigfinder.herokuapp.com/'
+																className='portfolio-link'
+															>
+																<h5>Live Link</h5>
+															</a>{' '}
+															<a
+																href='https://github.com/Gcriste/NewGigFinder'
+																className='portfolio-link'
+															>
 																{' '}
-																<a
-																	href='https://newgigfinder.herokuapp.com/'
-																	className='pichead'
-																>
-																	Live Link
-																</a>
-															</h5>
-															<h5>
-																{' '}
-																<a
-																	href='https://github.com/Gcriste/NewGigFinder'
-																	className='pichead'
-																>
-																	{' '}
-																	GitHub Link
-																</a>
-															</h5>
-															<p className='app-description'>
-																A Gig Finder app that helps musicians find available
-																gigs.
-															</p>
+																<h5>GitHub Link</h5>
+															</a>
 														</div>
+														<p className='app-description'>
+															A Gig Finder app that helps musicians find available gigs.
+														</p>
 													</div>
 												</div>
 											</div>
@@ -143,34 +130,31 @@ function Nav() {
 													<h4 className='text-center card-title'>
 														<strong>Math Whiz</strong>
 													</h4>
-													<div className='card-body'>
-														<a href='#demo4' data-toggle='collapse'>
-															<img
-																src={require('./assets/MathWhiz.png')}
-																className='person'
-																alt='pic of budget'
-															/>
-														</a>
-														<div id='demo4' className='collapse'>
-															<h5>
+												</div>
+												<div className='card-body'>
+													<a href='#demo4' data-toggle='collapse'>
+														<img
+															src={require('./assets/MathWhiz.png')}
+															className='person img-fluid'
+															alt='pic of budget'
+														/>
+													</a>
+													<div id='demo4' className='collapse'>
+														{' '}
+														<div className='link-container'>
+															<a href='/' className='portfolio-link'>
 																{' '}
-																<a href='/' className='pichead'>
-																	{' '}
-																	Live Link
-																</a>
-															</h5>
-															<h5>
+																<h5>Live Link</h5>
+															</a>{' '}
+															<a href='/' className='portfolio-link'>
 																{' '}
-																<a href='/' className='pichead'>
-																	{' '}
-																	GitHub Link{' '}
-																</a>
-															</h5>
-															<p className='app-description'>
-																A simple app to help children work on their addition,
-																subtraction, multiplication and division skills.
-															</p>
+																<h5>GitHub Link </h5>
+															</a>
 														</div>
+														<p className='app-description'>
+															A simple app to help children work on their addition,
+															subtraction, multiplication and division skills.
+														</p>
 													</div>
 												</div>
 											</div>
